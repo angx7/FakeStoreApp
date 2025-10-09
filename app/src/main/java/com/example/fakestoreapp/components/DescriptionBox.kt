7b823d97c4273453f1b98ac1ea231d845fc979e0
angx7 <angelmirocu@gmail.com>
2025-10-09 17:13:02 -0600

    feat(ui): creación del componente DescriptionBox para mostrar la descripción del producto
    - Componente reutilizable con scroll vertical y altura máxima definida
    - Muestra la descripción del producto usando el modelo Product
    - Aplica estilo tipográfico del tema (bodyMedium)
    - Mejora la legibilidad y modularidad dentro de ProductDetailScreen

--- app/src/main/java/com/example/fakestoreapp/components/DescriptionBox.kt
+++ app/src/main/java/com/example/fakestoreapp/components/DescriptionBox.kt
@@ -1,2 +1,30 @@
 package com.example.fakestoreapp.components
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.fakestoreapp.models.Product
+
+@Composable
+fun DescriptionBox(p: Product){
+    val scrollState = rememberScrollState()
+
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .heightIn(max = 300.dp)
+            .verticalScroll(scrollState)
+    ) {
+        Text(
+            text = p.description,
+            style = MaterialTheme.typography.bodyMedium
+        )
+    }
+}
