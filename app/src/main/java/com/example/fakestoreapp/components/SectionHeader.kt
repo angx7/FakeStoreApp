e58505310aa420a4ad7b31e53221ae47bf105cca
angx7 <angelmirocu@gmail.com>
2025-10-09 08:26:02 -0600

    feat(ui): creación del componente reutilizable SectionHeader
    - Componente genérico para encabezados de sección
    - Acepta parámetros dinámicos: título y acción (por ejemplo, "see more" o "view all")
    - Implementado con MaterialTheme y tipografía adaptable
    - Usado en secciones como Hot Sales, Select Category y Recently Viewed

---
+++ app/src/main/java/com/example/fakestoreapp/components/SectionHeader.kt
@@ -0,0 +1,30 @@
+package com.example.fakestoreapp.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.example.fakestoreapp.ui.theme.TextGray
+
+@Composable
+fun SectionHeader(title: String, action: String) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = title,
+            style = MaterialTheme.typography.titleMedium
+        )
+        Text(
+            text = action,
+            style = MaterialTheme.typography.bodyMedium,
+            color = TextGray
+        )
+    }
+}
